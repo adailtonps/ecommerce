9a5fe0da9367460270cb30f660fe659b392a220d
adailton@ps.com <adailtonpereirasilveira9@gmail.com>
2026-08-30 01:54:10 -0300

    consertando a verificação de endereço vazio

--- backend/src/main/java/com/adps/e_commerce/dto/AtualizarStatusPagamentoDTO.java
+++ backend/src/main/java/com/adps/e_commerce/dto/AtualizarStatusPagamentoDTO.java
@@ -6,6 +6,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -13,4 +15,5 @@ import lombok.Setter;
 public class AtualizarStatusPagamentoDTO {
     private String idPedido;
     private StatusPedido statusPagamento;
+    private LocalDateTime dataPagamento;
 }
